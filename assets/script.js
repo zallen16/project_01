29beb186caa69164013229d893bb81f5f998d54c
Aleah White <aleahwhite55@gmail.com>
2023-12-09 13:21:15 -0600

    added comments to js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,17 @@
-console.log('hello')
+// variables for api url, api key if needed, and certain parameters
+// variables for search button
+// any other variables needed
+
+// add event listener to search button to fetch data
+
+// fetch api data
+
+// console log the results
+
+// put results on page in html cards
+
+// make sort by function
+
+// make function to save shopping list items to local storage
+// then displaying those items on the page in a html card
+
