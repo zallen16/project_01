1c297dd003d9fb87c9775ac209ae4e58b70087b3
Aleah White <aleahwhite55@gmail.com>
2023-12-13 20:36:33 -0600

    added local storage feature to save button on shopping list

--- assets/script.js
+++ assets/script.js
@@ -15,3 +15,26 @@
 // make function to save shopping list items to local storage
 // then displaying those items on the page in a html card
 
+document.addEventListener('DOMContentLoaded', function () {
+    var savedValue = localStorage.getItem('inputValue');
+
+    var inputElement = document.getElementById('inputElement');
+    if (inputElement) {
+        inputElement.value = savedValue || '';
+    }
+
+    var saveButton = document.getElementById('saveButton');
+
+    saveButton.addEventListener('click', function () {
+        var inputElement = document.getElementById('inputElement');
+
+        if (inputElement) {
+            var inputValue = inputElement.value;
+
+            localStorage.setItem('inputValue', inputValue);
+
+            console.log('Input value:', inputValue);
+        }
+    });
+});
+
